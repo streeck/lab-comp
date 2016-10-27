4d82bcb036fac75d9d3ea0c4ace3c0afbdb1277e
angelarf <angela.rod.fe@gmail.com>
2016-10-27 15:57:26 -0200

    Metodos de encapsulamento add na classe KraClass

--- krakatoa/src/ast/KraClass.java
+++ krakatoa/src/ast/KraClass.java
@@ -15,7 +15,46 @@ public class KraClass extends Type {
    private String name;
    private KraClass superclass;
    private InstanceVariableList instanceVariableList;
+   private MethodList publicMethodList;
+   private MethodList privateMethodLis;
+
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public KraClass getSuperclass() {
+      return superclass;
+   }
+
+   public void setSuperclass(KraClass superclass) {
+      this.superclass = superclass;
+   }
+
+   public InstanceVariableList getInstanceVariableList() {
+      return instanceVariableList;
+   }
+
+   public void setInstanceVariableList(InstanceVariableList instanceVariableList) {
+      this.instanceVariableList = instanceVariableList;
+   }
+
+   public MethodList getPublicMethodList() {
+      return publicMethodList;
+   }
+
+   public void setPublicMethodList(MethodList publicMethodList) {
+      this.publicMethodList = publicMethodList;
+   }
+
+   public MethodList getPrivateMethodLis() {
+      return privateMethodLis;
+   }
+
+   public void setPrivateMethodLis(MethodList privateMethodLis) {
+      this.privateMethodLis = privateMethodLis;
+   }
    // private MethodList publicMethodList, privateMethodList;
-   // métodos públicos get e set para obter e iniciar as variáveis acima,
-   // entre outros métodos
+   // metodos publicos get e set para obter e iniciar as variaveis acima,
+   // entre outros metodos
 }

eb5f93209d417c4ef77e66e4693eedf023a006a9
angelarf <angela.rod.fe@gmail.com>
2016-10-28 00:35:13 -0200

    Classe StatementList add na AST

--- krakatoa/src/ast/StatementList.java
+++ krakatoa/src/ast/StatementList.java
@@ -1,7 +1,29 @@
 package ast;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Angela on 28/10/2016.
  */
 public class StatementList {
+    private ArrayList<Statement> stmtList;
+
+    public StatementList(){
+       this.stmtList = new ArrayList<Statement>();
+    }
+    public void addElement(Statement stmt) {
+        stmtList.add(stmt);
+    }
+
+    public Iterator<Statement> elements() {
+        return this.stmtList.iterator();
+    }
+
+    public int getSize() {
+        return stmtList.size();
+    }
+
+
+
 }

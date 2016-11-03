2b67beea2f32c835f0f20619087c982ce6bb3d55
angelarf <angela.rod.fe@gmail.com>
2016-11-03 20:34:30 -0200

    feat: Class ObjectConstructor created

--- krakatoa/src/ast/ObjectConstructor.java
+++ krakatoa/src/ast/ObjectConstructor.java
@@ -1,7 +1,38 @@
+//Angela Rodrigues Ferreira 552070
+//Charles David de Moraes 489662
 package ast;
 
-/**
- * Created by Angela on 03/11/2016.
- */
-public class ObjectConstructor {
+public class ObjectConstructor extends Expr{
+    private KraClass object;
+    private KraClass castingObj;
+
+    public ObjectConstructor(KraClass k){
+        this.setObject(k);
+   }
+
+    @Override
+    public void genC(PW pw, boolean putParenthesis) {
+
+    }
+
+    @Override
+    public Type getType() {
+        return getObject();
+    }
+
+    public KraClass getObject() {
+        return object;
+    }
+
+    public void setObject(KraClass object) {
+        this.object = object;
+    }
+
+    public KraClass getCastingObj() {
+        return castingObj;
+    }
+
+    public void setCastingObj(KraClass castingObj) {
+        this.castingObj = castingObj;
+    }
 }

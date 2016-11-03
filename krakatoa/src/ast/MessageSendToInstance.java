235e40bc7ecbafa4b52a136bbc5bd942825382d8
Charles David <raverblazer@gmail.com>
2016-11-03 21:47:49 -0200

    feat: add MessageSendToInstance class

---
+++ krakatoa/src/ast/MessageSendToInstance.java
@@ -0,0 +1,28 @@
+package ast;
+
+public class MessageSendToInstance extends Expr {
+
+    private Variable var;
+    private Variable instanceVariable;
+
+    public MessageSendToInstance(Variable inLocal, Variable instanceVariable) {
+        this.var = inLocal;
+        this.instanceVariable = instanceVariable;
+    }
+
+    @Override
+    public void genC(PW pw, boolean putParenthesis) {
+        pw.print(var.getName() + "." + instanceVariable.getName());
+    }
+
+//    @Override
+//    public void genKra(PW pw, boolean putParenthesis) {
+//        pw.printIdent(var.getName() + "." + instanceVariable.getName());
+//    }
+
+    @Override
+    public Type getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}

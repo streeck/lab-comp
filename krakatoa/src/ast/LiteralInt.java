378a8aca198221cacc4a9afff9850df19a65f2a9
Charles David <raverblazer@gmail.com>
2016-12-19 01:41:42 -0200

    fix: quick fix in LiteralInt

--- krakatoa/src/ast/LiteralInt.java
+++ krakatoa/src/ast/LiteralInt.java
@@ -13,7 +13,7 @@ public class LiteralInt extends Expr {
         return value;
     }
     public void genC( PW pw, boolean putParenthesis ) {
-        pw.printIdent("" + value);
+        pw.print(String.valueOf(value));
     }
 
     @Override
